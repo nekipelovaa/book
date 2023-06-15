c1d835f8a0bedc37d950972d2a60aa1eb05ef5e7
Aleksey N <blindfoldeah@gmail.com>
2023-06-15 23:24:30 +0300

    add dup2.go

--- 1/dup2.go
+++ 1/dup2.go
@@ -31,23 +31,40 @@ func main() {
 			indexOfFile |= n[1]
 		}
 	}
-	for i, s := range files {
-		if (1<<i)&indexOfFile != 0 {
-			fmt.Print(s)
+	if indexOfFile != 0 {
+		f := false
+		for i, s := range files {
+			if (1<<i)&indexOfFile != 0 {
+				if f {
+					fmt.Print(", ", s)
+					continue
+				}
+				fmt.Print(s)
+				f = true
+			}
 		}
 	}
 }
+
+// Подсчет строк с сохранением в counts
 func countLines(f *os.File, counts map[string][2]int, indexOfFile int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		// counts[input.Text()][0]++
 		// counts[input.Text()][1] |= indexOfFile
-		if counts[input.Text()] == [2]int{0, 0} {
-			counts[input.Text()] = [2]int{1, indexOfFile}
-		} else {
-			counts[input.Text()][0]++
-			counts[input.Text()][1] |= indexOfFile
-		}
+		temp := counts[input.Text()]
+		temp[0]++
+		temp[1] |= indexOfFile
+		counts[input.Text()] = temp
+		// if counts[input.Text()] == [2]int{0, 0} {
+		// 	counts[input.Text()] = [2]int{1, indexOfFile}
+		// } else {
+		// 	d := counts[input.Text()]
+		// 	d[0]++
+		// 	d[1] |= indexOfFile
+		// counts[input.Text()][0]++
+		// counts[input.Text()][1] |= indexOfFile
+		//}
 
 	}
 }
--- 1/try.go
+++ 1/try.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var i int = 1
-	i |= 8
-	fmt.Println(fmt.Sprint(i))
+	c := make(map[string][2]int)
+	// if c["s"] == [2]int{0, 0} {
+	// d := c["s"]
+	// d[0]++
+
+	// c["s"] = d
+	c["s"] = [2]int{0, 0}
+	c["s"][0] = 1
+	// }
+	fmt.Println(fmt.Sprint(c["s"][0]))
+	//fmt.Println(fmt.Sprint(d))
 }
